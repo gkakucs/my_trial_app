450b68ea372cee0cf1346663fa4c402c7449a318
gkakucs <gergokakucs@gmail.com>
2022-03-28 12:25:18 +0300

    Toast lib added

--- lib/SplashScreen.dart
+++ lib/SplashScreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:motion_toast/motion_toast.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -9,6 +11,28 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  final Connectivity _connectivity = Connectivity();
+
+  Future<void> CheckInternet() async {
+    ConnectivityResult connectivityResult = await _connectivity.checkConnectivity();
+
+    if ((connectivityResult == ConnectivityResult.mobile) || (connectivityResult == ConnectivityResult.wifi)) {
+      MotionToast.success(
+        title: const Text('Connected to network'),
+        description: const Text(
+          'Network connection OK',
+        ),
+      ).show(context);
+    } else {
+      MotionToast.error(
+        title: const Text('Error'),
+        description: const Text(
+          'No Network connection',
+        ),
+      ).show(context);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,7 +45,17 @@ class _SplashScreenState extends State<SplashScreen> {
               const SpinKitRing(
                 color: Colors.blue,
                 size: 50.0,
-              )
+              ),
+              ElevatedButton(
+                  onPressed: () => {
+                    MotionToast.success(
+                      title: const Text('Test'),
+                      description: const Text(
+                        'Desc',
+                      ),
+                    ).show(context)
+                  },
+                  child: Text("Toast")),
             ],
           )),
     );
@@ -31,6 +65,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     // TODO: implement initState
     super.initState();
+    CheckInternet();
   }
 
   @override
