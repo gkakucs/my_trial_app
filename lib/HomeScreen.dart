8e6c7372c1bb40e9d3c6a6751f1355b5f7f5b515
gkakucs <gergokakucs@gmail.com>
2022-03-29 17:01:07 +0300

    Final

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -14,143 +14,143 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'HomeScreen',
-      home: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text("My Trial App"),
-            bottom: const TabBar(
-              tabs: [
-                Tab(child: Text("Movie List")),
-                Tab(child: Text("Favorites")),
-                Tab(child: Text("User data")),
+        title: 'HomeScreen',
+        home: DefaultTabController(
+          length: 3,
+          child: Scaffold(
+            appBar: AppBar(
+              title: const Text("My Trial App"),
+              bottom: const TabBar(
+                tabs: [
+                  Tab(child: Text("Movie List")),
+                  Tab(child: Text("Favorites")),
+                  Tab(child: Text("User data")),
+                ],
+              ),
+            ),
+            body: TabBarView(
+              children: [
+                Column(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text("Movies"),
+                    ),
+                    SizedBox(
+                      height: 100,
+                      child: ListView(
+                        children: userData.userMovies.map((e) => Text(e.Name)).toList(growable: false),
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Expanded(
+                      child: ListView(
+                        shrinkWrap: true,
+                        children: userData.userMovies
+                            .map((e) => Card(
+                                    child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  mainAxisSize: MainAxisSize.max,
+                                  children: [
+                                    Padding(
+                                        padding: const EdgeInsets.all(10.0),
+                                        child: Image.asset(
+                                          'assets/logo.png',
+                                          scale: 1,
+                                        )),
+                                    Text(e.Name),
+                                    const Padding(
+                                      padding: EdgeInsets.all(2),
+                                      child: Icon(
+                                        Icons.favorite,
+                                        color: Colors.red,
+                                        size: 10,
+                                      ),
+                                    )
+                                  ],
+                                )))
+                            .toList(growable: false),
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(0, 0, 5, 0),
+                            child: Text("Username:"),
+                          ),
+                          SizedBox(
+                            width: 150,
+                            height: 35,
+                            child: TextField(
+                              obscureText: false,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Username',
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(0, 0, 5, 0),
+                            child: Text("Password:"),
+                          ),
+                          SizedBox(
+                            width: 150,
+                            height: 35,
+                            child: TextField(
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Password',
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(1, 10, 1, 10),
+                      child: ElevatedButton(
+                        child: const Text("Change Password"),
+                        onPressed: () => {},
+                      ),
+                    )
+                  ],
+                ),
               ],
             ),
           ),
-          body: TabBarView(
-            children: [
-              Column(
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Text("Movies"),
-                  ),
-                  SizedBox(
-                    height: 100,
-                    child: ListView(
-                      children: userData.userMovies.map((e) => Text(e.Name)).toList(growable: false),
-                    ),
-                  ),
-                ],
-              ),
-              Column(
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  Expanded(
-                    child: ListView(
-                      shrinkWrap: true,
-                      children: userData.userMovies
-                          .map((e) =>
-                          Card(
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                mainAxisSize: MainAxisSize.max,
-                                children: [
-                                  Padding(
-                                      padding: const EdgeInsets.all(10.0),
-                                      child: Image.asset(
-                                        'assets/logo.png',
-                                        scale: 1,
-                                      )),
-                                  Text(e.Name),
-                                  const Padding(
-                                    padding: EdgeInsets.all(2),
-                                    child: Icon(
-                                      Icons.favorite,
-                                      color: Colors.red,
-                                      size: 10,
-                                    ),
-                                  )
-                                ],
-                              )))
-                          .toList(growable: false),
-                    ),
-                  ),
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(0, 0, 5, 0),
-                          child: Text("Username:"),
-                        ),
-                        SizedBox(
-                          width: 150,
-                          height: 35,
-                          child: TextField(
-                            obscureText: false,
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: 'Username',
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(0, 0, 5, 0),
-                          child: Text("Password:"),
-                        ),
-                        SizedBox(
-                          width: 150,
-                          height: 35,
-                          child: TextField(
-                            obscureText: true,
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: 'Password',
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(1, 10, 1, 10),
-                    child: ElevatedButton(
-                      child: const Text("Change Password"),
-                      onPressed: ()=>{},
-                    ),
-                  )
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+        ));
   }
 
   @override
   void initState() {
     super.initState();
+
   }
 
   @override
   void dispose() {
     super.dispose();
+
   }
 }
