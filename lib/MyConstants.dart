2bf1b0417e20f237ff0231fd7dc95f5d66c180a3
gkakucs <gergokakucs@gmail.com>
2022-03-31 16:41:05 +0300

    Account ID

--- lib/MyConstants.dart
+++ lib/MyConstants.dart
@@ -1,7 +1,7 @@
 class MyConstants{
 
   static const String API_Key = "fe6b7eef3bf894c96bd84f9cdb34bdf4";
-  static const String ACCOUNT_ID="";
+  static const String ACCOUNT_ID="12134724";
 
   MyConstants();
 
